8d178827f1eea05394c20a63613b334b5f555747
Tom Meredith <tommeredith01@gmail.com>
2020-06-27 18:12:22 -0400

    chore: api and prediction script

--- api.py
+++ api.py
@@ -1,18 +1,14 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request
+from flask_restful import reqparse
 import pandas as pd
 import psycopg2
-from psycopg2.extensions import AsIs
 from psycopg2.extras import RealDictCursor
 from predictions import run_predictions
 import json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-api = Api(app)
-
-
-# df = pd.read_csv('league_table_premier.csv')
-# standings_dict = df.to_dict(orient='index')
+CORS(app, resources={r"/predict": {"origins": "http://localhost:8080"}})
 
 
 def connect_to_db():
@@ -21,52 +17,50 @@ def connect_to_db():
     return conn
 
 
-class Standings(Resource):
+@app.route('/standings', methods=['GET'])
+def fetch_standings():
 
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("league")
-        parser.add_argument("week")
-        params = parser.parse_args()
-
-        conn = connect_to_db()
-        select_statement = 'select * from standings_' + params["league"]
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        cursor.execute(select_statement)
-        standings_dict = cursor.fetchall()
-        conn.commit()
-        conn.close()
-        return standings_dict, 200
+    req_league = request.get_json()['league']
+    req_week = request.get_json()['week']
+    conn = connect_to_db()
+    select_statement = 'select * from standings_' + req_league
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(select_statement)
+    standings_dict = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return standings_dict, 200
 
 
-class Predictions(Resource):
+@app.route('/predict', methods=['POST'])
+@cross_origin(origin='localhost')
+def fetch_prediction():
+    req_league = request.get_json()['league']
+    req_week = request.get_json()['week']
 
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("league")
-        parser.add_argument("week")
-        params = parser.parse_args()
+    conn = connect_to_db()
+    select_match_stats_statement = 'select * from full_match_stats_' + req_league
+    select_standings_statement = 'select * from standings_' + req_league
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(select_match_stats_statement)
+    match_stats = pd.DataFrame(data=cursor.fetchall())
+    cursor.execute(select_standings_statement)
+    standings = pd.DataFrame(data=cursor.fetchall())
+    conn.commit()
+    conn.close()
 
-        conn = connect_to_db()
-        select_match_stats_statement = 'select * from full_match_stats_' + params["league"]
-        select_standings_statement = 'select * from standings_' + params["league"]
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        cursor.execute(select_match_stats_statement)
-        match_stats = pd.DataFrame(data=cursor.fetchall())
-        cursor.execute(select_standings_statement)
-        standings = pd.DataFrame(data=cursor.fetchall())
-        conn.commit()
-        conn.close()
-
-        predictions = run_predictions(params["week"], match_stats, standings)
-        predictions_dict = predictions.to_json(orient='index')
-        predictions_json = json.loads(predictions_dict)
-        return predictions_json, 200
+    predictions = run_predictions(req_week, match_stats, standings)
+    predictions_dict = predictions.to_json(orient='index')
+    predictions_json = json.loads(predictions_dict)
+    return predictions_json, 200
 
 
-api.add_resource(Standings, '/standings/')
-api.add_resource(Predictions, '/predict/')
+@app.after_request
+def after_request_func(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    print('this ran')
+    return response
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- predictions.py
+++ predictions.py
@@ -2,50 +2,113 @@ import pandas as pd
 import numpy as np
 from odds import calculate_odds
 
+home_coefs = {
+    "home_shots_coef": 1,
+    "home_shots_on_target_coef": 1.5,
+    "home_touches_coef": 1.4,
+    "home_pass_pct_coef": 1.3,
+    "home_expected_goals_coef": 1.5
+}
+
+away_coefs = {
+    "away_shots_coef": 1,
+    "away_shots_on_target_coef": 1.4,
+    "away_touches_coef": 1.3,
+    "away_pass_pct_coef": 1.2,
+    "away_expected_goals_coef": 1.4
+}
+
 
 def run_predictions(week, match_stats, standings):
     sims_to_run = 100000
-    df = pd.DataFrame(
-        columns=["week", "home_team", "away_team", "home_wins", "away_wins", "draws", "home_score", "away_score",
-                 "home_odds", "away_odds", "draw_odds"])
-    # only use games before the game_week we want to predict
-    game_week = int(week)
-    historical = match_stats.loc[match_stats["wk"] < game_week]
+    week = int(week)
+    df = pd.DataFrame(columns=["week", "home_team", "away_team", "home_wins", "away_wins", "draws", "home_score", "away_score", "home_odds", "away_odds", "draw_odds"])
+    # only use games before the week we want to predict
+    historical = match_stats.loc[match_stats["wk"] < week]
     # make sure we only use games that have valid scores
     historical = historical.loc[match_stats["home_score"] > -1]
 
     # get average home and away scores for entire competition
-    home_avg = historical["home_score"].mean() * historical["home_expected_goals"].mean()
-    away_avg = historical["away_score"].mean() * historical["away_expected_goals"].mean()
+    home_avg = historical["home_score"].mean() * (historical["home_expected_goals"].mean() * home_coefs["home_expected_goals_coef"])
+    away_avg = historical["away_score"].mean() * (historical["away_expected_goals"].mean() * away_coefs["away_expected_goals_coef"])
+
+    home_avg_shots = historical['home_shots'].mean() * home_coefs["home_shots_coef"]
+    home_avg_shots_on_target = historical['home_shots_on_target'].mean() * home_coefs["home_shots_on_target_coef"]
+    home_avg_touches = historical['home_touches'].mean() * home_coefs["home_touches_coef"]
+    home_avg_pass_pct = historical['home_pass_pct'].mean() * home_coefs["home_pass_pct_coef"]
+    away_avg_shots = historical['away_shots'].mean() * away_coefs["away_shots_coef"]
+    away_avg_shots_on_target = historical['away_shots_on_target'].mean() * away_coefs["away_shots_on_target_coef"]
+    away_avg_touches = historical['away_touches'].mean() * away_coefs["away_touches_coef"]
+    away_avg_pass_pct = historical['away_pass_pct'].mean() * away_coefs["away_pass_pct_coef"]
+
+    home_stats_avg = (home_avg_shots_on_target / home_avg_shots) + home_avg_touches + home_avg_pass_pct
+    away_stats_avg = (away_avg_shots_on_target / away_avg_shots) + away_avg_touches + away_avg_pass_pct
 
     # games to predict
-    to_predict = match_stats.loc[match_stats["wk"] == game_week]
+    to_predict = match_stats.loc[match_stats["wk"] == week]
     # loop through predicting games
     another_i = 1
 
     for i in to_predict.index:
+
         home_team = to_predict.loc[i, "home_team"]
         away_team = to_predict.loc[i, "away_team"]
         home_team_id = to_predict.loc[i, "home_id"]
         away_team_id = to_predict.loc[i, "away_id"]
 
+        home_team_shots_for = historical.loc[match_stats["home_id"] == home_team_id, "home_shots"].mean() * \
+                             home_coefs["home_shots_coef"]
+        home_team_shots_against = historical.loc[match_stats["home_id"] == home_team_id, "away_shots"].mean() * \
+                                 away_coefs["away_shots_coef"]
+        home_team_shots_on_target_for = historical.loc[match_stats["home_id"] == home_team_id, "home_shots_on_target"].mean() * home_coefs[
+            "home_shots_on_target_coef"]
+        home_team_shots_on_target_against = historical.loc[match_stats["home_id"] == home_team_id, "away_shots_on_target"].mean() * \
+                                away_coefs["away_shots_on_target_coef"]
+        home_team_touches_for = historical.loc[match_stats["home_id"] == home_team_id, "home_touches"].mean() * home_coefs[
+            "home_touches_coef"]
+        home_team_touches_against = historical.loc[match_stats["home_id"] == home_team_id, "away_touches"].mean() * \
+                                away_coefs["away_touches_coef"]
+        home_team_pass_pct_for = historical.loc[match_stats["home_id"] == home_team_id, "home_pass_pct"].mean() * home_coefs[
+            "home_pass_pct_coef"]
+        home_team_pass_pct_against = historical.loc[match_stats["home_id"] == home_team_id, "away_pass_pct"].mean() * \
+                                away_coefs["away_pass_pct_coef"]
+
+        away_team_shots_for = historical.loc[match_stats["away_id"] == away_team_id, "away_shots"].mean() * \
+                             away_coefs["away_shots_coef"]
+        away_team_shots_against = historical.loc[match_stats["away_id"] == away_team_id, "home_shots"].mean() * \
+                                 home_coefs["home_shots_coef"]
+        away_team_shots_on_target_for = historical.loc[match_stats["away_id"] == away_team_id, "away_shots_on_target"].mean() * away_coefs[
+            "away_shots_on_target_coef"]
+        away_team_shots_on_target_against = historical.loc[match_stats["away_id"] == away_team_id, "home_shots_on_target"].mean() * \
+                                home_coefs["home_shots_on_target_coef"]
+        away_team_touches_for = historical.loc[match_stats["away_id"] == away_team_id, "away_touches"].mean() * away_coefs[
+            "away_touches_coef"]
+        away_team_touches_against = historical.loc[match_stats["away_id"] == away_team_id, "home_touches"].mean() * \
+                                home_coefs["home_touches_coef"]
+        away_team_pass_pct_for = historical.loc[match_stats["away_id"] == away_team_id, "away_pass_pct"].mean() * away_coefs[
+            "away_pass_pct_coef"]
+        away_team_pass_pct_against = historical.loc[match_stats["away_id"] == away_team_id, "home_pass_pct"].mean() * \
+                                home_coefs["home_pass_pct_coef"]
+
         # average goals scored and goals conceded for home team
-        home_team_exp_goals_for = historical.loc[match_stats["home_id"] == home_team_id, "home_expected_goals"].mean()
-        home_team_exp_goals_against = historical.loc[match_stats["home_id"] == home_team_id, "away_expected_goals"].mean()
+        home_team_exp_goals_for = historical.loc[match_stats["home_id"] == home_team_id, "home_expected_goals"].mean() * home_coefs["home_expected_goals_coef"]
+        home_team_exp_goals_against = historical.loc[match_stats["home_id"] == home_team_id, "away_expected_goals"].mean() * away_coefs["away_expected_goals_coef"]
 
         # average goals scored and goals conceded for away team
-        away_team_exp_goals_for = historical.loc[match_stats["away_id"] == away_team_id, "away_expected_goals"].mean()
-        away_team_exp_goals_against = historical.loc[match_stats["away_id"] == away_team_id, "home_expected_goals"].mean()
+        away_team_exp_goals_for = historical.loc[match_stats["away_id"] == away_team_id, "away_expected_goals"].mean() * away_coefs["away_expected_goals_coef"]
+        away_team_exp_goals_against = historical.loc[match_stats["away_id"] == away_team_id, "home_expected_goals"].mean() * home_coefs["home_expected_goals_coef"]
 
-        home_team_expected_to_actual_for = standings.loc[standings["team_id"] == home_team_id, "goals_for"].sum() / standings.loc[
-            standings["team_id"] == home_team_id, "xg_for"].sum()
-        home_team_expected_to_actual_against = standings.loc[standings["team_id"] == home_team_id, "goals_against"].sum() / \
-                                               standings.loc[standings["team_id"] == home_team_id, "xg_against"].sum()
+        home_team_expected_to_actual_for = standings.loc[standings["team_id"] == home_team_id, "goals_for"].sum() / standings.loc[standings["team_id"] == home_team_id, "xg_for"].sum()
+        home_team_expected_to_actual_against = standings.loc[standings["team_id"] == home_team_id, "goals_against"].sum() / standings.loc[standings["team_id"] == home_team_id, "xg_against"].sum()
 
-        away_team_expected_to_actual_for = standings.loc[standings["team_id"] == away_team_id, "goals_for"].sum() / standings.loc[
-            standings["team_id"] == away_team_id, "xg_for"].sum()
-        away_team_expected_to_actual_against = standings.loc[standings["team_id"] == away_team_id, "goals_against"].sum() / \
-                                               standings.loc[standings["team_id"] == away_team_id, "xg_against"].sum()
+        away_team_expected_to_actual_for = standings.loc[standings["team_id"] == away_team_id, "goals_for"].sum() / standings.loc[standings["team_id"] == away_team_id, "xg_for"].sum()
+        away_team_expected_to_actual_against = standings.loc[standings["team_id"] == away_team_id, "goals_against"].sum() / standings.loc[standings["team_id"] == away_team_id, "xg_against"].sum()
+
+        home_team_offensive_stats = (home_team_shots_on_target_for / home_team_shots_for) + home_team_touches_for + home_team_pass_pct_for
+        home_team_defensive_stats = (home_team_shots_on_target_against / home_team_shots_against) + home_team_touches_against + home_team_pass_pct_against
+
+        away_team_offensive_stats = (away_team_shots_on_target_for / away_team_shots_for) + away_team_touches_for + away_team_pass_pct_for
+        away_team_defensive_stats = (away_team_shots_on_target_against / away_team_shots_against) + away_team_touches_against + away_team_pass_pct_against
 
         # calculate home and away offense and defense strength
         home_team_offense_strength = (home_team_exp_goals_for * home_team_expected_to_actual_for) / home_avg
@@ -53,6 +116,18 @@ def run_predictions(week, match_stats, standings):
 
         away_team_offense_strength = (away_team_exp_goals_for * away_team_expected_to_actual_for) / away_avg
         away_team_defense_strength = (away_team_exp_goals_against * away_team_expected_to_actual_against) / home_avg
+
+        # ============== noting off for now until can get appropriate stats ===============
+
+        # home_team_offense_strength = ((home_team_exp_goals_for * home_team_expected_to_actual_for) + home_team_offensive_stats) / (
+        #                                          home_avg + home_stats_avg)
+        # home_team_defense_strength = ((home_team_exp_goals_against * home_team_expected_to_actual_against) + home_team_defensive_stats) / (
+        #                                          away_avg + away_stats_avg)
+        #
+        # away_team_offense_strength = ((away_team_exp_goals_for * away_team_expected_to_actual_for) + away_team_offensive_stats) / (
+        #                                          away_avg + away_stats_avg)
+        # away_team_defense_strength = ((away_team_exp_goals_against * away_team_expected_to_actual_against) + away_team_defensive_stats) / (
+        #                                          home_avg + home_stats_avg)
 
         home_team_expected_goals = home_team_offense_strength * away_team_defense_strength * home_avg
         away_team_expected_goals = away_team_offense_strength * home_team_defense_strength * away_avg
@@ -72,7 +147,7 @@ def run_predictions(week, match_stats, standings):
         draw_odds = calculate_odds(draws, 'favorite') if draws > 50 else calculate_odds(draws, 'dog')
 
         df.loc[another_i] = {
-            "week": game_week,
+            "week": week,
             "home_team": home_team,
             "away_team": away_team,
             "home_wins": home_wins,
